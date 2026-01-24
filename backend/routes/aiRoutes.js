228f65ef343814228ce73b0a81d546f1d2630e93
Prem <premkumarsingh355@gmail.com>
2026-01-24 20:10:29 +0530

    ai

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,13 +1,26 @@
 import express from "express";
+import fs from "fs";
 
 const router = express.Router();
 
 router.post("/generate-video", (req, res) => {
+  const { courseTitle, celebrity } = req.body;
+  //generate video by calling ai_service
+  
+  
   setTimeout(() => {
-    res.json({
-      videoUrl: "http://localhost:5000/videos/modiji.mp4",
-      captions: "This is a sample REACTJS lesson video."
-    });
+    fs.readFile(`./videos/${celebrity}.mp4`, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: "Failed to read video file" });
+      } else {
+        console.log("Video file read successfully");
+        res.json({
+          videoUrl: `http://localhost:5000/videos/${celebrity}.mp4`,
+          captions: `This is a sample ${courseTitle} lesson video.`
+        })
+      }
+    })
   }, 1000);
 });
 
